0f71156864f005cd4f6f50fbd79ec589d7f3850e
qaanit <qaanitb@gmail.com>
2025-02-27 13:33:53 +0200

    just another test

--- testFromQ.java
+++ testFromQ.java
@@ -5,6 +5,7 @@ class testFromQ{
 
     public static void main(String[] args){
 
-        System.out.println("hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Bye world!");
     }
 }
